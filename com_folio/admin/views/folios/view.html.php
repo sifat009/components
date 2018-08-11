a71a6e3450defb78021704d8d867e3cd248ced09
sifat009 <bhacker150@gmail.com>
2018-08-11 13:55:16 -0700

    added folios view file in admin

---
+++ com_folio/admin/views/folios/view.html.php
@@ -0,0 +1,20 @@
+<?php
+    defined('_JEXEC') or die;
+
+    class FolioViewFolios extends JViewLegacy {
+        protected $items;
+        public function display($tpl = null) {
+            $this->items = $this->get('Items');
+
+            if(count($errors = $this->get('Errors'))) {
+                JError::raiseError(500, implode("\n", $errors));
+                return false;
+            }
+
+            $this->addToolbar();
+            parent::display($tpl);
+        }
+
+        protected function addToolbar()
+    }
+?>
