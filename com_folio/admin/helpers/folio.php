c94942739bb4b67fdb61cac48d77efcdcc93427d
sifat009 <bhacker150@gmail.com>
2018-08-31 00:20:45 -0700

    added helper code in admin/helpers/folio.php

--- com_folio/admin/helpers/folio.php
+++ com_folio/admin/helpers/folio.php
@@ -1,3 +1,27 @@
 <?php
+    define('_JEXEC') or die;
 
+    class FolioHelper {
+
+        public static function getActions($categoryId = 0) {
+            $user = JFactory::getUser();
+            $result = new JObject();
+    
+            if(empty($categoryId)) {
+                $assetName = 'com_folio';
+                $level = 'component';
+            } else {
+                $assetName = 'com_folio.category.'. (int) $categoryId;
+                $level = 'category';
+            }
+    
+            $actions = JAccess::getActions('com_folio', $level);
+            foreach($actions as $action) {
+                $result->set($action->name, $user->authorise($action->name, $assetName));
+            }
+
+            return $result;
+        }
+
+    }
 ?>
