1f6386a6c35ee37737dbe8b31861261e5a5fad05
sifat009 <sifathaque6@gmail.com>
2018-09-03 17:19:14 +0600

    added the edit view and scriipt.php

---
+++ com_folio/admin/views/folio/tmpl/edit.php
@@ -0,0 +1,26 @@
+<?php
+    defined('_JEXEC') or die;
+?>
+
+<form action="<?php echo JRoute::_('index.php?option=com_folio&layout=edit&id='.(int) $this->item->id); ?>" method="post" name="adminForm" id="adminForm" class="form-validate">
+    <div class="row-fluid">
+        <div class="span10 form-horizontal">
+            <fieldset>
+                <?php echo JHtml::_('bootstrap.startPane', 'myTab', array('active' => 'details')); ?>
+                <?php echo JHtml::_('bootstrap.addPanel', 'myTab', 'details', empty($this->item->id) ? JText::_('COM_FOLIO_NEW_FOLIO', true) : JText::sprintf('COM_FOLIO_EDIT_FOLIO', $this->item->id, true)); ?>
+                    <div class="control-group">
+                        <div class="control-label">
+                            <?php echo $this->form->getLabel('title'); ?>
+                        </div>
+                        <div class="controls">
+                            <?php echo $this->form->getInput('title'); ?>
+                        </div>
+                    </div>
+                <?php echo JHtml::_('bootstrap.endPanel'); ?>
+                <input type="hidden" name="task" value="" />
+                <?php echo JHtml::_('form.token'); ?>
+                <?php echo JHtml::_('bootstrap.endPane'); ?>
+            </fieldset>
+        </div>
+    </div>
+</form>
