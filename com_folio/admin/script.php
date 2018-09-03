15ea494b82de240b8136451c6729f538ceaf6772
sifat009 <sifathaque6@gmail.com>
2018-09-03 18:01:38 +0600

    added language file for script.php

--- com_folio/admin/script.php
+++ com_folio/admin/script.php
@@ -12,10 +12,10 @@
             echo '<p>'. JText::_('COM_FOLIO_UPDATE_TEXT') .'</p>';
         }
         function preflight($type, $parent) {
-            echo '<p>'. JText::_('COM_FOLIO_PREFLIGHT_'. $type . '_TEXT') .'</p>';
+            echo '<p>'. JText::_('COM_FOLIO_PREFLIGHT_'. strtoupper($type) . '_TEXT') .'</p>';
         }
         function postflight($type, $parent) {
-            echo '<p>'. JText::_('COM_FOLIO_POSTFLIGHT_'. $type . '_TEXT') .'</p>';
+            echo '<p>'. JText::_('COM_FOLIO_POSTFLIGHT_'. strtoupper($type) . '_TEXT') .'</p>';
         }
     }
 ?>
