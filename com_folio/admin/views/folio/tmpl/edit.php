4d687fb7386bd7cc53795ba317f0f0cc18a4673c
sifat009 <bhacker150@gmail.com>
2018-09-10 01:38:22 -0700

    added single form view to input data

--- com_folio/admin/views/folio/tmpl/edit.php
+++ com_folio/admin/views/folio/tmpl/edit.php
@@ -16,6 +16,62 @@
                             <?php echo $this->form->getInput('title'); ?>
                         </div>
                     </div>
+                    <div class="control-group">
+                        <div class="control-label">
+                            <?php echo $this->form->getLabel('catid'); ?>
+                        </div>
+                        <div class="controls">
+                            <?php echo $this->form->getInput('catid'); ?>
+                        </div>
+                    </div>
+                    <div class="control-group">
+                        <div class="control-label">
+                            <?php echo $this->form->getLabel('published'); ?>
+                        </div>
+                        <div class="controls">
+                            <?php echo $this->form->getInput('published'); ?>
+                        </div>
+                    </div>
+                    <div class="control-group">
+                        <div class="control-label">
+                            <?php echo $this->form->getLabel('image'); ?>
+                        </div>
+                        <div class="controls">
+                            <?php echo $this->form->getInput('image'); ?>
+                        </div>
+                    </div>
+                    <div class="control-group">
+                        <div class="control-label">
+                            <?php echo $this->form->getLabel('company'); ?>
+                        </div>
+                        <div class="controls">
+                            <?php echo $this->form->getInput('company'); ?>
+                        </div>
+                    </div>
+                    <div class="control-group">
+                        <div class="control-label">
+                            <?php echo $this->form->getLabel('phone'); ?>
+                        </div>
+                        <div class="controls">
+                            <?php echo $this->form->getInput('phone'); ?>
+                        </div>
+                    </div>
+                    <div class="control-group">
+                        <div class="control-label">
+                            <?php echo $this->form->getLabel('url'); ?>
+                        </div>
+                        <div class="controls">
+                            <?php echo $this->form->getInput('url'); ?>
+                        </div>
+                    </div>
+                    <div class="control-group">
+                        <div class="control-label">
+                            <?php echo $this->form->getLabel('description'); ?>
+                        </div>
+                        <div class="controls">
+                            <?php echo $this->form->getInput('description'); ?>
+                        </div>
+                    </div>
                 <?php echo JHtml::_('bootstrap.endPanel'); ?>
                 <input type="hidden" name="task" value="" />
                 <?php echo JHtml::_('form.token'); ?>
